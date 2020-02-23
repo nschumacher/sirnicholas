b13c1c105301eef51948b544da8b49cb045eef3a
Nick Schumacher <nschumacher@appirio.com>
2020-02-23 15:51:12 -0500

    Added slugs to Blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_222214) do
+ActiveRecord::Schema.define(version: 2020_02_23_204721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_02_17_222214) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
 end
