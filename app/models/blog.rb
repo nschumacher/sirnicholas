08ba10c0ff66fbc6d69b09a59eef2e24c56a3b02
Nick Schumacher <nschumacher@appirio.com>
2020-04-04 16:51:09 -0400

    Added Tags and a little more
    Implemented tags gem. Added Topic, tags, and status fields to Blog.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,7 @@ class Blog < ApplicationRecord
     enum status: { draft: 0, published: 1 }
     extend FriendlyId
     friendly_id :title, use: :slugged
+    acts_as_taggable_on :tags
 
     belongs_to :topic
     
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,4 @@
 class Topic < ApplicationRecord
-
     has_many :blog
+    validates_presence_of :title
 end
