44afbc0ca7694d86f07fb955d9194b1e96d56f9f
Nicholas Schumacher <nick.schumacher07@gmail.com>
2022-07-09 17:53:08 -0400

    Created views for Topics, fixed Topic links on Blog index

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,15 @@
 class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+
+    if logged_in?(:site_admin)
+        @blogs = Blog.recent.where(topic_id: @topic.id).page(params[:page]).per(2) # for site admins to see all
+    else
+        @blogs = Blog.published.recent.where(topic_id: @topic.id).page(params[:page]).per(2) # for non-admins to see only published posts
+    end
+  end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,10 +4,10 @@ class Blog < ApplicationRecord
     friendly_id :title, use: :slugged
     acts_as_taggable_on :tags
 
+    validates_presence_of :title, :body, :topic_id
+
     belongs_to :topic
     has_many :comments, dependent: :destroy
-    
-    validates_presence_of :title, :body
 
     mount_uploader :main_image, VaultUploader # mount_uploader is a method provided by carrierwave
     mount_uploader :thumb_image, VaultUploader # it stores a link to the image, which is the goal
