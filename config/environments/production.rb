cd0f34e0b88f4920382f8e41cb8163f162616f44
Nicholas Schumacher <nick.schumacher07@gmail.com>
2022-08-15 13:41:57 -0400

    addressing config needs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://sirnicholas-io.herokuapp.com/', 'http://sirnicholas-io.herokuapp.com/']
+  config.action_cable.url = 'wss://sirnicholas-io.herokuapp.com/cable'
 end
